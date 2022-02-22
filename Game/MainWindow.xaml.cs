e9c1c7d2ece8bd0dd5552de7286ec612b100cdac
Buga-00 <cemal.buga@hotmail.com>
2022-02-22 20:06:39 +0100

    Made a WinCheck that starts a new game if no one wins and makes the background gold and whoever wins the row will turn pink, so the users will se who won and where they won.

--- Game/MainWindow.xaml.cs
+++ Game/MainWindow.xaml.cs
@@ -119,8 +119,105 @@ namespace Game
                 btn.Foreground = Brushes.Cyan;
             }
 
+            WinCheck();
         }
 
+        private void WinCheck()
+        {
 
+            #region Row wins
+
+            //Checking for the row wins
+            //First row
+            if ((theResults[0] & theResults[1] & theResults[2]) == theResults[0] && theResults[0] != Type.Free)
+            {
+                endedGame = true;
+
+                Btn0_0.Background = Btn1_0.Background = Btn2_0.Background = Brushes.Pink;
+            }
+
+            //Second Row
+            if ((theResults[3] & theResults[4] & theResults[5]) == theResults[3] && theResults[3] != Type.Free)
+            {
+                endedGame = true;
+
+                Btn0_1.Background = Btn1_1.Background = Btn2_1.Background = Brushes.Pink;
+            }
+
+            //Third row
+            if ((theResults[6] & theResults[7] & theResults[8]) == theResults[6] && theResults[6] != Type.Free)
+            {
+                endedGame = true;
+
+                Btn0_2.Background = Btn1_2.Background = Btn2_2.Background = Brushes.Pink;
+            }
+
+            #endregion
+
+            #region Column wins
+
+            //Checking for the column wins
+            //First column
+            if ((theResults[0] & theResults[3] & theResults[6]) == theResults[0] && theResults[0] != Type.Free)
+            {
+                endedGame = true;
+
+                Btn0_0.Background = Btn0_1.Background = Btn0_2.Background = Brushes.Pink;
+            }
+
+            //Second column
+            if ((theResults[1] & theResults[4] & theResults[7]) == theResults[1] && theResults[1] != Type.Free)
+            {
+                endedGame = true;
+
+                Btn1_0.Background = Btn1_1.Background = Btn1_2.Background = Brushes.Pink;
+            }
+
+            //Third column
+            if ((theResults[2] & theResults[5] & theResults[8]) == theResults[2] && theResults[2] != Type.Free)
+            {
+                endedGame = true;
+
+                Btn2_0.Background = Btn2_1.Background = Btn2_2.Background = Brushes.Pink;
+            }
+
+            #endregion
+
+            #region cross wins
+
+            //Top right to bottom left
+            if ((theResults[2] & theResults[4] & theResults[6]) == theResults[2] && theResults[2] != Type.Free)
+            {
+                endedGame = true;
+
+                Btn2_0.Background = Btn1_1.Background = Btn0_2.Background = Brushes.Pink;
+            }
+
+            //Top left to bottom right
+            if ((theResults[0] & theResults[4] & theResults[8]) == theResults[0] && theResults[0] != Type.Free)
+            {
+                endedGame = true;
+
+                Btn0_0.Background = Btn1_1.Background = Btn2_2.Background = Brushes.Pink;
+            }
+
+            #endregion
+
+            #region No one wins
+
+            if (!theResults.Any(r => r == Type.Free))
+            {
+                endedGame = true;
+
+                GridUI.Children.Cast<Button>().ToList().ForEach(btn =>
+                {
+                    btn.Background = Brushes.Gold;
+                });
+            }
+
+            #endregion
+
+
+        }
     }
 }
